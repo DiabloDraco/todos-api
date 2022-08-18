ec2952c96aa8438453e6e8a460c4363c2d249d16
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-08-18 18:38:27 +0500

    add

--- js/main.js
+++ js/main.js
@@ -8,15 +8,15 @@ let elWrapper = document.querySelector("#wrapper")
 
 
 fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(data => renderUsers(data))
+.then(res => res.json())
+.then(data => renderUsers(data))
 
 function renderUsers(array) {
     let fragment = document.createDocumentFragment()
-
+    
     for (let i = 0; i < array.length; i++) {
         let usersTemplate = elUsersTemplate.cloneNode(true)
-
+        
         usersTemplate.querySelector("#userItem").dataset.userID = array[i].id
         usersTemplate.querySelector("#usersName").textContent = array[i].name
         usersTemplate.querySelector("#usersName").dataset.userID = array[i].id
@@ -31,7 +31,7 @@ function renderUsers(array) {
         usersTemplate.querySelector("#usersWeb").textContent = array[i].website
         usersTemplate.querySelector("#usersWeb").dataset.userID = array[i].id
         usersTemplate.querySelector("#usersWeb").href = `${array[i].website}`
-
+        
         fragment.appendChild(usersTemplate)
     }
     elUsersList.appendChild(fragment)
@@ -39,18 +39,20 @@ function renderUsers(array) {
 
 elWrapper.addEventListener("click" , function (evt) {
     let current = evt.target.dataset
-
+    
     if (current.userID) {
         fetch(`https://jsonplaceholder.typicode.com/posts`)
-            .then(res => res.json())
-            .then(data => renderPost(data.filter(function (item) {
-               return current.userID == item.userId
-            })))
+        .then(res => res.json())
+        .then(data => renderPost(data.filter(function (item) {
+            return current.userID == item.userId
+        })))
     }
     if (current.postId) {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${current.postId}/comments`)
-            .then(res => res.json())
-            .then(data => renderComment(data))
+        fetch(`https://jsonplaceholder.typicode.com/comments`)
+        .then(res => res.json())
+        .then(data => renderComment(data.filter(function (item) {
+            return current.postId == item.postId
+        })))
     }
 })
 
@@ -62,13 +64,13 @@ function renderPost(array) {
     
     for (let i = 0; i < array.length; i++) {
         let postTemplate = elPostsTemplate.cloneNode(true)
-
+        
         postTemplate.querySelector("#postItem").dataset.postId = array[i].id
         postTemplate.querySelector("#postsTitle").textContent = array[i].title
         postTemplate.querySelector("#postsTitle").dataset.postId = array[i].id
         postTemplate.querySelector("#postsBody").textContent = array[i].body
         postTemplate.querySelector("#postsBody").dataset.postId = array[i].id
-
+        
         fragment.appendChild(postTemplate)
     }
     elPostsList.appendChild(fragment)
@@ -80,7 +82,7 @@ function renderComment(array) {
     
     for (let i = 0; i < array.length; i++) {
         let commentsTemplate = elCommentsTemplate.cloneNode(true)
-
+        
         commentsTemplate.querySelector("#commentItem").dataset.commentsId = array[i].id
         commentsTemplate.querySelector("#commentsName").textContent = array[i].name
         commentsTemplate.querySelector("#commentsName").dataset.commentsId = array[i].id
@@ -89,7 +91,7 @@ function renderComment(array) {
         commentsTemplate.querySelector("#commentsMail").dataset.commentsId = array[i].id
         commentsTemplate.querySelector("#commentsBody").textContent = array[i].body
         commentsTemplate.querySelector("#commentsBody").dataset.commentsId = array[i].id
-
+        
         fragment.appendChild(commentsTemplate)
     }
     elCommentsList.appendChild(fragment)
